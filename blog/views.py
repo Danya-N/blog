c719f39af36e0acaaf4c82ed1ca57eb050c167ce
Danya-N <danyakenobi@gmail.com>
2025-04-02 17:48:28 +0300

    .

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')  # Сортування від нового до старого
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def add_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')  # Повернення на головну сторінку після додавання
+    else:
+        form = PostForm()
+    return render(request, 'blog/add_post.html', {'form': form})
