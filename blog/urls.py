783b6866df8dadd2d5a5e2bade8a6ed7ab005692
Danya-N <danyakenobi@gmail.com>
2025-04-03 22:00:53 +0300

    Додав публікацію посту

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('add/', views.add_post, name='add_post'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),  # Новий маршрут для видалення
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
@@ -15,3 +15,8 @@ def add_post(request):
     else:
         form = PostForm()
     return render(request, 'blog/add_post.html', {'form': form})
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.delete()
+    return redirect('post_list')  # Повернення на головну сторінку після видалення
